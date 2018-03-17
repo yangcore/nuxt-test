74e9d8c1e4128cc00f5e892fddd7798d6af0475c
yangchao <yangchao03@paicaifu.com>
2018-03-17 15:06:10 +0800

    增加pm2 配置文件

---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  apps: [
+    {
+      name: 'NUXT',
+      script: './server/app.js',
+      env_dev: {
+        NODE_ENV: 'dev'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      log_date_format: "YYYY-MM-DD HH:mm Z"
+    }
+  ]
+  // deploy : {
+  //   production : {
+  //     user : 'node',
+  //     host : '212.83.163.1',
+  //     ref  : 'origin/master',
+  //     repo : 'git@github.com:repo.git',
+  //     path : '/var/www/production',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  //   },
+  //   dev : {
+  //     user : 'node',
+  //     host : '212.83.163.1',
+  //     ref  : 'origin/master',
+  //     repo : 'git@github.com:repo.git',
+  //     path : '/var/www/development',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+  //     env  : {
+  //       NODE_ENV: 'dev'
+  //     }
+  //   }
+  // }
+};
