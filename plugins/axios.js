30b4732d8412828942c305f68cb8e570395eacac
yangchao <yangchao03@paicaifu.com>
2018-03-17 15:23:27 +0800

    增加pm2 配置文件

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
 console.info(process.env.NODE_ENV)
-let baseURL;
+let baseURL = 'http://111.231.89.222:3030/';
 if (process.env.NODE_ENV === "development") {
   baseURL = 'http://127.0.0.1:3030/'
-} else {
-  baseURL: 'http://111.231.89.222:3030/';
 }
-
 var instance = axios.create({
   baseURL: baseURL,
   timeout: 30000
