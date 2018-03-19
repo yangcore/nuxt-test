3e23bc94f4d85d495c55c79a921d116bfec6e97a
yangchao <yangchao03@paicaifu.com>
2018-03-19 13:11:48 +0800

    更换公网ip

--- plugins/axios.js
+++ plugins/axios.js
@@ -5,7 +5,7 @@ let baseURL;
 if (process.env.NODE_ENV === "development") {
   baseURL = 'http://127.0.0.1:3030/'
 } else if (process.env.NODE_ENV === "production") {
-  baseURL = 'http://111.231.89.222:3030/';
+  baseURL = 'http://111.231.55.104:3030/';
 }
 var instance = axios.create({
   baseURL: baseURL,
